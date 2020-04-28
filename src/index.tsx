72723d5230374544c237c4fb23ce953c8b8c8b4f
Raí Siqueira <rai93siqueira@gmail.com>
2020-04-28 19:11:27 -0300

    fix: Fixed types
    Signed-off-by: Raí Siqueira <rai93siqueira@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,22 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
-import { UseViaCepHook, ViaCepResponse } from 'types';
+
+type ViaCepResponse = {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  unidade: string;
+  ibge: string;
+  gia: string;
+};
+
+type UseViaCepHook = {
+  cep: ViaCepResponse;
+  error: { message: string; error: any };
+  loading: boolean;
+};
 
 const useViaCep = (value: string | number = ''): UseViaCepHook => {
   const [cep, setCep] = useState<ViaCepResponse>({} as ViaCepResponse);
