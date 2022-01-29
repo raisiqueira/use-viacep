93e0ba226fb0f8f3e4da133f6d43da05b12a4c7f
Rai Siqueira <rai93siqueira@gmail.com>
2022-01-28 23:41:18 -0300

    feat: update bundle tool
    Signed-off-by: Rai Siqueira <rai93siqueira@gmail.com>

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,6 +1,1 @@
 import '@testing-library/jest-dom';
-import { server } from '../src/mocks/server';
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,23 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useViaCep from './index';
+
+const setup = () => {
+  const { result, rerender } = renderHook(({ value }) => useViaCep(value), {
+    initialProps: { value: '' },
+  });
+
+  return { result: result.current, rerender };
+};
+
 describe('use-viacep', () => {
-  it('should fetch a valid CEP', () => {
-    expect(true).toBeTruthy();
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should init with default values', () => {
+    const { cep, loading, error } = setup().result;
+    expect(cep).toEqual({});
+    expect(loading).toBe(false);
+    expect(error).toBe(null);
   });
 });
