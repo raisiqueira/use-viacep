7e676232a9f4d15d9272b6478cba73c1e743bc65
Raí Siqueira <me@raisiqueira.io>
2022-07-21 23:35:56 +0000

    chore: update example project

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,19 @@
-import 'react-app-polyfill/ie11';
+import { stringify } from 'querystring';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import useViaCep from '../src/index';
 
 const App = () => {
   const [data, setData] = React.useState('');
-  const {} = useViaCep(data);
+  const { cep } = useViaCep(data);
   return (
     <div>
       <input placeholder="cep" onChange={e => setData(e.target.value)} />
+      <div>
+        <pre>
+          {JSON.stringify(cep, null, 2)}
+        </pre>
+      </div>
     </div>
   );
 };
