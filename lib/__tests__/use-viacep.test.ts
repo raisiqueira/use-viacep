aadfd683b76e1eb130ceca405610ed1b626499b9
Raí Siqueira <rai93siqueira@gmail.com>
2022-08-22 20:38:56 -0300

    test: fix use viacep tests
    Signed-off-by: Raí Siqueira <rai93siqueira@gmail.com>

--- lib/__tests__/use-viacep.test.ts
+++ lib/__tests__/use-viacep.test.ts
@@ -1,26 +1,26 @@
-import { renderHook } from '@testing-library/react';
-import { rest } from 'msw';
+import { renderHook } from '@testing-library/react-hooks';
 import { describe, it, expect } from 'vitest';
 
-import { ERROR_RESPONSE, SUCCESS_RESPONSE, server } from '../mock/server';
+import { ERROR_RESPONSE, SUCCESS_RESPONSE } from '../mock/server';
 import useViaCep from '../src';
 
 describe('useViaCep', () => {
   const setupHook = (cep?: string) => {
     return renderHook(() => useViaCep(cep));
   };
-  it.skip('should return an object with the response', () => {
-    server.use(
-      rest.get('https://viacep.com.br/ws/50690708/json/', (req, res, ctx) => {
-        return res(ctx.status(200), ctx.json({}));
-      })
-    );
-    const { result } = setupHook('50690708');
+  it('should return an object with the response', async () => {
+    const { result, waitForNextUpdate } = setupHook('50690708');
+    expect(result.current.loading).toBeTruthy();
+    await waitForNextUpdate();
     expect(result.current.cep).toEqual(SUCCESS_RESPONSE);
+    expect(result.current.loading).toBeFalsy();
   });
 
-  it.skip("should return an object with the error if the cep doesn't exist", () => {
-    const { result } = setupHook('99999999');
+  it("should return an object with the error if the cep doesn't exist", async () => {
+    const { result, waitForNextUpdate } = setupHook('99999999');
+    expect(result.current.loading).toBeTruthy();
+    await waitForNextUpdate();
     expect(result.current.cep).toEqual(ERROR_RESPONSE);
+    expect(result.current.loading).toBeFalsy();
   });
 });
