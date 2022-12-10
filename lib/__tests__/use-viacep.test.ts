a887053d6bf303cb0578807c30f906d7c3cc0b79
Raí Siqueira <rai93siqueira@gmail.com>
2022-12-10 09:51:42 -0300

    chore: improve tests
    Signed-off-by: Raí Siqueira <rai93siqueira@gmail.com>

--- lib/__tests__/use-viacep.test.ts
+++ lib/__tests__/use-viacep.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, waitFor } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 
 import { ERROR_RESPONSE, SUCCESS_RESPONSE } from '../mock/server';
@@ -9,17 +9,17 @@ describe('useViaCep', () => {
     return renderHook(() => useViaCep(cep));
   };
   it('should return an object with the response', async () => {
-    const { result, waitForNextUpdate } = setupHook('50690708');
+    const { result } = setupHook('50690708');
     expect(result.current.loading).toBeTruthy();
-    await waitForNextUpdate();
+    await waitFor(() => expect(result.current.loading).toBeFalsy());
     expect(result.current.cep).toEqual(SUCCESS_RESPONSE);
     expect(result.current.loading).toBeFalsy();
   });
 
   it("should return an object with the error if the cep doesn't exist", async () => {
-    const { result, waitForNextUpdate } = setupHook('99999999');
+    const { result } = setupHook('99999999');
     expect(result.current.loading).toBeTruthy();
-    await waitForNextUpdate();
+    await waitFor(() => expect(result.current.loading).toBeFalsy());
     expect(result.current.cep).toEqual(ERROR_RESPONSE);
     expect(result.current.loading).toBeFalsy();
   });
